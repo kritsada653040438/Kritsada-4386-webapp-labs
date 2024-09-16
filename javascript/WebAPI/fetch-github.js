8c2d2a8d711f34fa7d450cb30c51d5cd1f18ff67
kritsada653040438 <kritsada.ma@kkumail.com>
2024-09-16 16:43:20 +0700

    fix calling API

--- javascript/WebAPI/fetch-github.js
+++ javascript/WebAPI/fetch-github.js
@@ -1,24 +1,28 @@
- // Function to fetch GitHub user information
- async function fetchGitHubInfo(username) {
-    try {
-      const response = await fetch(
-        `https://api.github.com/users/${username}`
-      );
-      const data = await response.json();
+// Function to fetch GitHub user information
+async function fetchGitHubInfo(username) {
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const data = await response.json();
 
-      // Handle case where 'name' is null or empty
-      const displayName = data.name ? data.name : data.login;
+    // Handle case where 'name' is null or empty
+    const displayName = data.name ? data.name : data.login;
 
-      //show github info
-      document.getElementById("name").innerHTML = `<a href="${data.html_url}" target="_blank">${displayName}</a>`;
-      document.getElementById("avatar-image").src = data.avatar_url;
-    } catch (error) {
-      console.error("Error fetching GitHub information:", error);
-    }
+    //show github info
+    document.getElementById(
+      "name"
+    ).innerHTML = `<a href="${data.html_url}" target="_blank">${displayName}</a>`;
+    var avatar = document.getElementById("avatar-image");
+    avatar.src = data.avatar_url; // กำหนดรูปภาพจาก URL
+    avatar.onclick = function () {
+      window.open(data.html_url); // เปิดหน้า Git เมื่อคลิกที่รูป
+    };
+  } catch (error) {
+    console.error("Error fetching GitHub information:", error);
   }
+}
 
-  // Call the function
-  window.onload = function () {
-    const username = "kritsada653040438";
-    fetchGitHubInfo(username);
-  };
+// Call the function
+window.onload = function () {
+  const username = "kritsada653040438";
+  fetchGitHubInfo(username);
+};
