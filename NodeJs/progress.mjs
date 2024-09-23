3ba24e8ac0d785259d547cec5200550e4204789f
kritsada6530404386 <kritsada.ma@kkumail.com>
2024-09-23 16:07:37 +0700

    Exercise 21 Problem 2 Basic Calculator

---
+++ NodeJs/progress.mjs
@@ -0,0 +1,8 @@
+import ProgressBar from "progress"
+const bar = new ProgressBar(":bar", { total: 50 })
+const timer = setInterval(() => {
+  bar.tick()
+  if (bar.complete) {
+    clearInterval(timer)
+  }
+}, 100)
